e992dc73389d8541d9e50bd77d89d5546b5ef972
Jerryh001 <jerryh001@hotmail.com>
2015-11-25 20:15:55 +0800

    減少比較次數 以一已知之好點比較其他點 將症狀產生移到尋找元件才進行

--- BubbleSortGraph/f_component.cpp
+++ BubbleSortGraph/f_component.cpp
@@ -7,6 +7,7 @@ void BGraph::f_comp()
 	{
 		if (i->Component_ID == NULL)
 		{
+			Point_Symptom_Get(*i);
 			list<BPoint*> todolist = { &*i };
 			Component.push_back( &*i );
 			i->Component_ID = &Component.back();
@@ -15,19 +16,20 @@ void BGraph::f_comp()
 			{
 				BPoint& head = *todolist.front();
 				todolist.pop_front();
+				
 				for (vector<Stauts>::const_iterator i = head.Neighbor.begin(); i != head.Neighbor.end(); i++)//檢查過去
 				{
 					if (i->Guess == false)//好點
 					{
 						BPoint& JPoint = GetPoint(i->ID);
 						if (JPoint.Component_ID != NULL) continue;
+						Point_Symptom_Get(JPoint);
 						vector<Stauts>::const_iterator& J = find(JPoint.Neighbor.begin(), JPoint.Neighbor.end(), Stauts{ head.ID,false });//檢查回來
 						if (J != JPoint.Neighbor.end())
 						{
 							JPoint.Component_ID = head.Component_ID;
 							JPoint.Component_ID->member.push_back(&JPoint);
 							todolist.push_back(&JPoint);
-							//CheckPointComp(JPoint);//會stackoverflow
 						}
 					}
 				}
@@ -48,51 +50,6 @@ void BGraph::f_comp()
 				Component.pop_back();
 			}
 		}
-		/*for (list<BPoint>::iterator i = Isolated_Neighbor.begin(); i != Isolated_Neighbor.end();i++) {
-		//找孤立點附近的component
-		}*/
-			//先放著以防萬一
-			/*for (vector<Stauts>::iterator j = i->Neighbor.begin(); j != i->Neighbor.end(); j++)
-			{
-				if (j->Guess == false)
-				{
-					BPoint &pointJ = GetPoint(j->ID);
-					for (vector<Stauts>::iterator k = pointJ.Neighbor.begin(); k != pointJ.Neighbor.end(); k++)
-					{
-						if (k->ID == i->ID)//改用find做
-						{
-							if (k->Guess == false)//這裡感覺怪怪的
-							{
-								if (i->Component_ID == NULL && pointJ.Component_ID == NULL)
-								{
-									Component.push_back({ &*i ,&pointJ });
-									i->Component_ID = pointJ.Component_ID = &Component.back();
-								}
-								else if (i->Component_ID == NULL)
-								{
-									i->Component_ID=pointJ.Component_ID;
-									pointJ.Component_ID->push_back(&*i);
-								}
-								else if (pointJ.Component_ID == NULL)
-								{
-									 pointJ.Component_ID= i->Component_ID;
-									 i->Component_ID->push_back(&pointJ);
-								}
-								else if (i->Component_ID != pointJ.Component_ID)//連起來
-								{
-									list<BPoint*>* temp = pointJ.Component_ID;//可能沒問題了
-									for (list<BPoint*>::iterator l = temp->begin(); l != temp->end(); l++)
-									{
-										(*l)->Component_ID = i->Component_ID;
-									}
-									i->Component_ID->splice(i->Component_ID->end(),*temp);
-								}
-							}
-							break;
-						}
-					}
-				}
-			}*/
 	}
 }
 void BGraph::FindGoodComp()
@@ -112,26 +69,5 @@ void BGraph::FindGoodComp()
 				}
 			}
 		}
-		
 	}
 }
-/*
-void BGraph::CheckPointComp(const BPoint& B,list<BPoint>& todolist)//BFS(吧)
-{
-	//if (B.Component_ID != NULL) return;
-	for (vector<Stauts>::const_iterator i = B.Neighbor.begin(); i != B.Neighbor.end(); i++)//檢查過去
-	{
-		if (i->Guess == false)//好點
-		{
-			BPoint& JPoint = GetPoint(i->ID);
-			if (JPoint.Component_ID != NULL) continue;
-			vector<Stauts>::const_iterator& J = find(JPoint.Neighbor.begin(), JPoint.Neighbor.end(), Stauts{ B.ID,false });//檢查回來
-			if (J != JPoint.Neighbor.end())
-			{
-				JPoint.Component_ID = B.Component_ID;
-				JPoint.Component_ID->push_back(&JPoint);
-				CheckPointComp(JPoint);//會stackoverflow
-			}
-		}
-	}
-}*/
